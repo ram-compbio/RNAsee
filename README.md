4d6840acdf2b43c0148c81bffb5b2790d7e954a4
zfalls <zfalls@me.com>
2018-09-18 10:17:54 -0400

    Cleaned up/commented code. Added tri- and penta-loop functionality. Stores in dataframe now and outputs df to cl.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # RNAsee
+
+Must have RNAfold installed (ViennaRNA - https://www.tbi.univie.ac.at/RNA/)
--- dna2rna.py
+++ dna2rna.py
@@ -1,30 +1,54 @@
 import sys
+import pandas as pd
 
 # Function to convert DNA sequence to RNA sequence
 def dna2rna(dna):
     rna = []
     # clean DNA str
     dna = dna.lower().replace(' ','').replace('\n','')
-    #print(dna)
+    print(dna)
     #print(dna[::-1])
     # create rna sequence with complements to dna sequence
     for i in dna:
         if i == 't':
-            rna += ['a']
-        elif i == 'a':
             rna += ['u']
+        elif i == 'a':
+            rna += ['a']
         elif i == 'g':
-            rna += ['c']
-        elif i == 'c':
             rna += ['g']
+        elif i == 'c':
+            rna += ['c']
         else:
             print("Unknown nucleotide {}...exiting program".format(i))
             sys.exit()
-    rna = rna[::-1]
     rna = ''.join(rna)
     return rna
 
-def pallindrome(rna,length,i,j):
+
+# Function to convert RNA sequence to DNA sequence
+def rna2dna(rna):
+    dna = []
+    # clean DNA str
+    rna = rna.lower().replace(' ','').replace('\n','')
+    # create rna sequence with complements to dna sequence
+    for i in rna:
+        if i == 'u':
+            dna += ['t']
+        elif i == 'a':
+            dna += ['a']
+        elif i == 'g':
+            dna += ['g']
+        elif i == 'c':
+            dna += ['c']
+        else:
+            print("Unknown nucleotide {}...exiting program".format(i))
+            sys.exit()
+    dna = ''.join(dna)
+    return dna
+
+# Function to determine the length of the pallindromic sequence
+# Stem length
+def pallindrome(rna,loop,length,i,j,columns):
     temp_i = i-1
     temp_j = j+1
     stem = 0
@@ -43,45 +67,65 @@ def pallindrome(rna,length,i,j):
             #print rna[temp_i:temp_j+1]
         else:
             break
+        # Keep searching...
         temp_i -= 1
         temp_j += 1
-    if stem < 4:
+    if stem < 3:
         return
     temp_i += 1
     temp_j -= 1
-    print ''.join(rna[temp_i:temp_j+1])
-    print("Stem length = {}".format(stem))
-    print("End positions of stem-loop sequence are {}, {}\n".format(temp_i,temp_j))
+    # Temp store stem-loop sequence
+    temp_rna = rna[temp_i:temp_j+1]
+    # Get dna complement of rna stem-loop sequence
+    temp_dna = rna2dna(temp_rna)
+    # Store all stem-loop info
+    df = pd.DataFrame([[temp_rna,temp_dna,loop,stem,temp_i,temp_j]], columns=columns)
+    return df
 
 
 
+#f = open("sdhb-wt.fasta",'r')
 #f = open("sdhb-partial_cds.fasta",'r')
-#f = open("sdhb-complete_cds.fasta",'r')
-f = open("chromosome-1.fasta",'r')
+#f = open("pcgf3-mrna.fasta",'r')
+f = open("sdhb-complete_cds.fasta",'r')
+#f = open("chromosome-1.fasta",'r')
 f.readline()
 l = f.readlines()
 dna = ''.join(l)
-dna = dna.lower().replace(' ','').replace('\n','')
+#dna = dna.lower().replace(' ','').replace('\n','')
 
 # Input was DNA
 # Convert to RNA
 rna = dna2rna(dna)
 print rna
 
+# Create dataframe
+columns=('rna','dna','loop','stem_len','begin','end')
+df = pd.DataFrame(columns=columns)
+
 size = len(rna)
 i = 0
 j = i+3
 while j < size:
     if rna[j] == 'c':
-        #print rna[i:j+1]
-#        if rna[i:j+1] == 'uauc':
         if rna[i:j+1] == 'cauc' \
                 or rna[i:j+1] == 'cacc' \
                 or rna[i:j+1] == 'ccuc' \
                 or rna[i:j+1] == 'cuuc' \
                 or rna[i:j+1] == 'uauc':
-            pallindrome(rna,size,i,j)
-#        elif rna[j-1] == 'u':
-#            pallindrome(rna,size,i,j)
+            loop = 4
+            df = df.append(pallindrome(rna,loop,size,i,j,columns), ignore_index=True)
+        else:
+            tetra = 0
+            # I should check for tri- and penta-loops here
+            shift_i = i+1
+            loop = 3
+            df = df.append(pallindrome(rna,loop,size,shift_i,j,columns), ignore_index=True)
+            shift_i = i-1
+            loop = 5
+            df = df.append(pallindrome(rna,loop,size,shift_i,j,columns), ignore_index=True)
     i += 1
     j += 1
+# Sort df by stem length (descending)
+df = df.sort_values(by=['stem_len'], ascending=False)
+print df
