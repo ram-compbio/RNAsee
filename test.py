6212074e280d9828b2233c33bdfcffdc154e51bd
Melissa Van Norden <51799604+MelissaVanNorden@users.noreply.github.com>
2023-07-27 16:13:17 -0400

    Updated test
    Made test use current file/function names; ensured that each major function (rules-based, ML, union, intersection) received a test

--- test.py
+++ test.py
@@ -1,9 +1,31 @@
 #!/usr/bin/env python
+test_files = ['test/app-cds.fasta', 'test/sdhb-cds.fasta', 'test/xpo1-cds.fasta']
 
-import rna_see
+import rules_based_methods
 
-rna_see.see("test/app-cds.fasta")
-rna_see.see("test/sdhb-cds.fasta")
-rna_see.see("test/xpo1-cds.fasta")
+print('Running rules-based tests...')
+
+for file in test_files:
+  print(rules_based_methods.scan_gene(file, threshold=9,
+                                      make_output=False)['pos_c'].values)
+
+import ML_methods
+
+print('\nRunning ML tests...')
+
+for file in test_files:
+  print(ML_methods.scan_gene('current_model.pickle', file))
+
+import consensus
+
+print('\nRunning union tests...')
+
+for file in test_files:
+  print(consensus.union(file))
+
+print('\nRunning intersection tests...')
+
+for file in test_files:
+  print(consensus.intersection(file))
 
 print("Done")
